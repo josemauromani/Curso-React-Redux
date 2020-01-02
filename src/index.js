5be9c39739dde3c7b6623b19150489eb2d3f8c32
josemauromani <josemauromani@gmail.com>
2020-01-02 15:29:33 -0300

    master template

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const elemento = document.getElementById('root')
-ReactDOM.render(<h1>Olá React com React</h1>,elemento)
+ReactDOM.render(<h1>Olá React </h1>,elemento)
