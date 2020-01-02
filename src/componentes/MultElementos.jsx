1a4800740d79a5831a9ce52ff891c8a4922ffd2e
josemauromani <josemauromani@gmail.com>
2020-01-02 17:35:27 -0300

    mutiplos elementos

---
+++ src/componentes/MultElementos.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default props => [
+    <h1>Parte 1</h1>,
+    <h2>Parte 2</h2>
+]
+
+
+    // SOLUÇÃO 02
+    // <React.Fragment>
+    //     <h1>Parte 1</h1>
+    //     <h2>Parte 2</h2>
+    // </React.Fragment>
+
+    // SOLUÇÃO 01 
+    // <div>
+    //     <h1>Parte 1</h1>
+    //     <h2>Parte 2</h2>
+    // </div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import PrimeiroComponente from "./componentes/PrimeiroComponente";
-import { CompA,CompB as B } from './componentes/DoisComponentes'
+import MultElementos from './componentes/MultElementos'
 
 const elemento = document.getElementById("root");
 ReactDOM.render(
   <div>
-    <CompA valor="Olá eu sou o A"></CompA>
-    <B valor="B na área"></B>
-    {/* <PrimeiroComponente valor="1"></PrimeiroComponente> */}
-    {/* <PrimeiroComponente valor="2"></PrimeiroComponente> */}
-    {/* <PrimeiroComponente valor="3" calculo={Math.pow(2, 3)}></PrimeiroComponente> */}
+    <MultElementos></MultElementos>
   </div>,
   elemento
 );
