d12e465427df5e42f42c3c50a41feec0f9c90d56
v8software <developer@v8software.com.br>
2020-01-26 08:24:19 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import FamiliaMani from './componentes/FamiliaMani'
+import Famlia from './componentes/Familia'
+import Membro from './componentes/Membro'
+import Familia from "./componentes/Familia";
 
 const elemento = document.getElementById("root");
 ReactDOM.render(
   <div>
-    <FamiliaMani></FamiliaMani>
+    <Familia>
+      <Membro nome="José Mauro Morais" sobrenome="Mani"></Membro>
+      <Membro nome="Isabel Ramos" sobrenome="Mani"></Membro>
+    </Familia>
+    <Familia>
+      <Membro nome="Edvaldo" sobrenome="Ramos"></Membro>
+      <Membro nome="Edson" sobrenome="Ramos"></Membro>
+    </Familia>
   </div>,
   elemento
 );
